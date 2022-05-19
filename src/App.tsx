8385ca1d67c6f857b500f1806d8b0d584932cacb
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-05-19 15:42:10 -0300

    chore: add miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react'
+
+// interface Todo  {
+//   description: string
+//   completed: boolean
+// }
+
 function App() {
+  const [todo, setTodo]= useState(null)
+  useEffect(()=>{
+    fetch('http://localhost:3000/api/todos')
+    .then(res=>res.json())
+    .then(data=>console.log('get',data))
+
+  },[])
   return(
     <h1>Routes</h1>
   )
