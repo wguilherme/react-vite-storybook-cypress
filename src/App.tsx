99e0cebdf40d01b7976b4ace33490f76177c9257
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-05-19 15:45:15 -0300

    chore: add axios and api service

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { useEffect, useState } from 'react'
-
-// interface Todo  {
-//   description: string
-//   completed: boolean
-// }
+import { useEffect } from 'react'
+import { api } from './services/api'
 
 function App() {
-  const [todo, setTodo]= useState(null)
   useEffect(()=>{
-    fetch('http://localhost:3000/api/todos')
-    .then(res=>res.json())
-    .then(data=>console.log('get',data))
+    api.get('http://localhost:3000/api/todos').then(response=>{
+      console.log('response.data',response.data)
+    })
 
   },[])
   return(
