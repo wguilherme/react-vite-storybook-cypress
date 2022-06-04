9a4d0d706cbcbc22c366e4bcbb58e1b22e90c7cf
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-06-04 20:49:16 -0300

    feat: add screen test from rtl

--- src/components/button/Button.spec.tsx
+++ src/components/button/Button.spec.tsx
@@ -1,9 +1,10 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Button } from './Button';
 test('button renders correctly', () =>{
   // expect(1+1).toBe(3);
   const {debug} = render(
-    <Button label="Confirmar"/>
+    <Button label="Confirmei"/>
   )
+  expect(screen.getByText('Confirmei')).toBeInTheDocument();
   debug()
 })
