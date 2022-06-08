ce69019ba1f510a2c5d08be9b77f3df972fcebb5
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-06-08 18:45:00 -0300

    chore: add react-query lib and refactor user data list to query data

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import UserList from './pages/users'
+import {QueryClient, QueryClientProvider} from 'react-query'
 
-
+const queryClient = new QueryClient
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
     <Routes>
       <Route path="/" element={<App/>}/>
       <Route path="/users" element={<UserList/>}/>
       </Routes>
+    </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,30 +1,42 @@
+import { makeServer } from '../../services/mirage'
 import { useEffect, useState } from 'react'
 import { api } from '../../services/api'
-import { makeServer } from '../../services/mirage'
+import {useQuery} from 'react-query'
 
 if(process.env.NODE_ENV === 'development') {makeServer()}
 
 export default function UserList(){
-  let [users, setUsers] = useState([])
+  const {data, isLoading, error} = useQuery('users', async()=>{
+    const {data} = await api.get('/users')
+    const users = data.users.map((user: any)=>{
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString(
+          'pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+          }
+        ),
 
-  useEffect(() => {
-    async function fetchUsers(){
-      try{
-        const {data} = await api.get('/users')
-        setUsers(data?.users)
-      }catch(error){
-        console.log(error)
       }
-    }
-    fetchUsers()
+    })
+    return users
+  })
 
-  }, [])
+  if(isLoading) return <p>Loading...</p>
   return (
     <div>
       <h2>Users</h2>
       <ul>
-        {users?.map((user:any)=>(
-          <li key={user.id}>{user.name}</li>
+        {data.map((user:any)=>(
+          <li key={user.id} style={{padding: '10px', lineHeight: '5px'}}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{user.createdAt}</p>
+            </li>
         ))}   
       </ul>
     </div>
