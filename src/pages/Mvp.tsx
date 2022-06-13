cdb33ba5b27ff8fa447f7ae245521dccd2dddd27
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-06-13 14:54:08 -0300

    feat: add Echarts react

--- src/pages/Mvp.tsx
+++ src/pages/Mvp.tsx
@@ -1,7 +1,34 @@
-import { Typography } from '@mui/material';
+
 import Header from './../components/HeaderPage'
+import echarts from 'echarts';
+import ReactECharts from 'echarts-for-react';
+
 export default function Mvp() {
+  const options = {
+    grid: { top: 8, right: 8, bottom: 24, left: 36 },
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        smooth: true,
+      },
+    ],
+    tooltip: {
+      trigger: 'axis',
+    },
+  };
+  
   return (
+    <>
     <Header title="MVPs" />
+    <ReactECharts option={options} />
+    </>
   )
 }
